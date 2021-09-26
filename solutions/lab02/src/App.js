1c86a366ad12cd6a9e93d1c508dfc751ae374009
Chris Minnick <chris@minnick.com>
2021-09-26 10:28:25 -0700

    added lab02

--- solutions/lab02/src/App.js
+++ solutions/lab02/src/App.js
@@ -1,15 +1,17 @@
+import Footer from './Footer.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h1>Welcome to React Bookstore</h1>
         <p>
           We have several books. Feel free to browse for as long as you like. Click on a cover image to see details, or click the Add to Cart button to add a book to your shopping cart.
         </p>
         
       </header>
+      <Footer />
     </div>
   );
 }
--- todo.md
+++ todo.md
@@ -1,3 +1,4 @@
 # Plans for future improvements
 
 1. Use Docker?
+1. Make a detailed schedule with suggested timing
