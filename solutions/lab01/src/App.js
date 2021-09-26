b5f49dd05eebe32989ed1f40871e8de0e64055b9
Chris Minnick <chris@minnick.com>
2021-09-26 10:39:02 -0700

    added lab03

--- solutions/lab01/src/App.js
+++ solutions/lab01/src/App.js
@@ -4,12 +4,13 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Welcome to React Bookstore</h1>
+        <h1>Welcome to React Bookstore</h1>        
+      </header>
+      <main>
         <p>
           We have several books. Feel free to browse for as long as you like. Click on a cover image to see details, or click the Add to Cart button to add a book to your shopping cart.
         </p>
-        
-      </header>
+      </main>
     </div>
   );
 }
