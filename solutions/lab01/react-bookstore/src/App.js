49c238b959140c3b30c14f36f61028ed82ff6f15
Chris Minnick <chris@minnick.com>
2021-09-26 09:38:08 -0700

    added lab01

---
+++ solutions/lab01/react-bookstore/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to React Bookstore</h1>
+        <p>
+          We have several books. Feel free to browse for as long as you like. Click on a cover image to see details, or click the Add to Cart button to add a book to your shopping cart.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
