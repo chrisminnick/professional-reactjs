adbe807d67228d22c45312a1a096cc9eb94f88eb
Chris Minnick <chris@minnick.com>
2021-10-12 01:44:10 -0700

    added lab14

---
+++ solutions/lab14/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.css';
+import reportWebVitals from './reportWebVitals';
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import {cart, products} from './reducers';
+
+const rootReducer = combineReducers({
+	cart: cart,
+	products: products
+});
+
+
+const initialState = {
+  cart: {items:[]},
+  products: {products:[]}
+};
+
+let store = createStore(
+	rootReducer,
+	initialState
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
