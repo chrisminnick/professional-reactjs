82b1ff474e3cb24158ca0f05aa024fb26549c519
Chris Minnick <chris@minnick.com>
2021-11-17 15:50:46 -0800

    modified tests in 13

--- solutions/lab13/src/components/CartItem.test.js
+++ solutions/lab13/src/components/CartItem.test.js
@@ -1,18 +1,15 @@
-import CartItem from './CartItem';
 import {shallow} from 'enzyme';
+import CartItem from './CartItem';
 
 let component;
-
 beforeEach(() => {
-    component =
-        shallow(<CartItem
-                    title="test book"
-                    price="6" />);
-});
-
+    component = 
+      shallow(<CartItem 
+              title="test book" 
+              price="6" />);
+  });
+  
 it('renders without crashing', () => {
-	
 	expect(component.text())
 		.toEqual('test book - 6');
-
 });
