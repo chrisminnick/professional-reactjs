043ffade3a7df847ea0bf4ea2e875bb27069f34b
Chris Minnick <chris@minnick.com>
2022-05-10 11:48:04 -0700

    added test to main

--- solutions/lab07/src/components/Main.test.js
+++ solutions/lab07/src/components/Main.test.js
@@ -2,7 +2,21 @@ import { render, screen } from '@testing-library/react';
 import Main from './Main';
 
 test('renders testing text', () => {
-  render(<Main />);
-  const testText = screen.getByText(/We have several books./i);
+  const products = [
+    {
+      id: '1',
+      title: 'Things Fall Apart',
+      author: 'Chinua Achebe',
+      published: '1958',
+      country: 'Nigeria',
+      lang: 'English',
+      pages: '209',
+      image: 'things-fall-apart.jpg',
+      url: 'https://en.wikipedia.org/wiki/Things_Fall_Apart',
+      price: '5',
+    },
+  ];
+  render(<Main products={products} />);
+  const testText = screen.getByText(/Things Fall Apart/i);
   expect(testText).toBeInTheDocument();
 });
