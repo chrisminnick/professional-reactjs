3b92e5133fefd2ff582020647df93f04eed2c74f
Chris Minnick <chris@minnick.com>
2022-06-15 12:12:46 -0700

    formatting files

--- solutions/lab10/src/components/App.js
+++ solutions/lab10/src/components/App.js
@@ -1,37 +1,39 @@
-import {Component} from 'react';
-import Header from './Header'
-import Main from './Main'
+import { Component } from 'react';
+import Header from './Header';
+import Main from './Main';
 import Footer from './Footer';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       itemsInCart: [],
       products: [],
-      loading: false
-    }
+      loading: false,
+    };
     this.addToCart = this.addToCart.bind(this);
     this.removeFromCart = this.removeFromCart.bind(this);
   }
 
   componentDidMount() {
-    this.setState({loading:true})
-    fetch('http://localhost:3000/data/products.json')
-            .then(response => response.json()
-            .then(products => this.shuffleArray(products))
-            .then(products => {
-                this.setState({products:products,loading:false});
-            }))
-    };
+    this.setState({ loading: true });
+    fetch('http://localhost:3000/data/products.json').then((response) =>
+      response
+        .json()
+        .then((products) => this.shuffleArray(products))
+        .then((products) => {
+          this.setState({ products: products, loading: false });
+        })
+    );
+  }
 
   shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     }
     return array;
   }
@@ -39,25 +41,25 @@ class App extends Component {
   addToCart(id) {
     let newItems = [...this.state.itemsInCart, id];
     this.setState({
-        itemsInCart: newItems
-    })
+      itemsInCart: newItems,
+    });
   }
 
   removeFromCart(idToRemove) {
-    let newItems = this.state.itemsInCart.filter(
-          id => id !== idToRemove);
-    this.setState({itemsInCart: newItems});
-  }  
+    let newItems = this.state.itemsInCart.filter((id) => id !== idToRemove);
+    this.setState({ itemsInCart: newItems });
+  }
 
-  render(){ 
+  render() {
     return (
       <div className="container">
         <Header />
-        <Main products = {this.state.products}
-              itemsInCart = {this.state.itemsInCart} 
-              addToCart = {this.addToCart}
-              removeFromCart = {this.removeFromCart}
-              />
+        <Main
+          products={this.state.products}
+          itemsInCart={this.state.itemsInCart}
+          addToCart={this.addToCart}
+          removeFromCart={this.removeFromCart}
+        />
         <Footer />
       </div>
     );
