b1c542820c4733f314f96225cc6b98652b9975f4
Chris Minnick <chris@minnick.com>
2024-03-28 11:44:41 -0500

    adding makepdf example to lab 11

---
+++ solutions/lab11/src/components/MakePdf.jsx
@@ -0,0 +1,37 @@
+import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+import ReactDOM from 'react-dom/client';
+import { PDFViewer } from '@react-pdf/renderer';
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#E4E4E4',
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
+});
+
+// Create Document Component
+const MyDocument = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+);
+
+export const App = () => (
+  <PDFViewer>
+    <MyDocument />
+  </PDFViewer>
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
