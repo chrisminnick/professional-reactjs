227a88261de70e228076338c3c02da3d2529c7f4
Chris Minnick <chris@minnick.com>
2025-02-17 13:13:20 -0800

    fixing main file name

--- solutions/lab03/src/main.jsx
+++ solutions/lab03/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './components/App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
