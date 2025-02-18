8e189129c94316df3d50cd8dd17ca327fa306866
Chris Minnick <chris@minnick.com>
2025-02-18 08:17:14 -0800

    new labs 4-7

--- react-bookstore/src/components/ProductList.test.jsx
+++ react-bookstore/src/components/ProductList.test.jsx
@@ -4,9 +4,23 @@ import '@testing-library/jest-dom/vitest';
 import ProductList from './ProductList.jsx';
 
 describe('ProductList Component', () => {
+  const book = {
+    id: '1',
+    title: 'Fake Title',
+    price: '10',
+    author: 'Author',
+    published: 'Published Date',
+    country: 'Country',
+    lang: 'Language',
+    pages: '100',
+    image: 'image.jpg',
+    url: 'Link URL',
+    inCart: false,
+    avgRating: '5',
+  };
   it('Renders', () => {
-    render(<ProductList />);
-    let element = screen.getAllByText(/title/i);
+    render(<ProductList products={[book]} />);
+    let element = screen.getAllByText(/Fake Title/i);
     expect(element[0]).toBeInTheDocument();
   });
 });
