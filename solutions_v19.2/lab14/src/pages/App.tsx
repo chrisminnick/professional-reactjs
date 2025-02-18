8abb5116b601223cab46748ce1b4e3655008f801
Chris Minnick <chris@minnick.com>
2025-02-18 15:14:52 -0800

    updating new solutions

---
+++ solutions_v19.2/lab14/src/pages/App.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import Header from '../components/Header.jsx';
+import ProductList from '../components/ProductList.jsx';
+import Cart from '../components/Cart.jsx';
+import Footer from '../components/Footer.jsx';
+import './App.css';
+import useBooks from '../hooks/useBooks.js';
+import { Book } from '../../types/book';
+
+function App() {
+  const [itemsInCart, setItemsInCart] = useState<Array<Book>>([]);
+  const [products, isLoading] = useBooks();
+
+  function addToCart(product: Book) {
+    let newItems = [...itemsInCart, product];
+    setItemsInCart(newItems);
+  }
+  function removeFromCart(idToRemove: string) {
+    let newItems = itemsInCart.filter((item) => item.id !== idToRemove);
+    setItemsInCart(newItems);
+  }
+
+  if (isLoading) {
+    return 'Loading...';
+  } else {
+    return (
+      <div className="container">
+        <Header />
+        <div className="row">
+          <div className="col-md-8">
+            <ProductList
+              addToCart={addToCart}
+              removeFromCart={removeFromCart}
+              itemsInCart={itemsInCart}
+              products={products}
+            />
+          </div>
+          <div className="col-md-4">
+            <Cart itemsInCart={itemsInCart} />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
