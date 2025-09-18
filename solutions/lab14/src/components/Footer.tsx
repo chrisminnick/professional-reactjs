a5c13a82aef0bd0a7d69c013373a3328602e34ee
Chris Minnick <chris@minnick.com>
2025-09-18 11:58:30 -0700

    fixing the footer

--- solutions/lab14/src/components/Footer.tsx
+++ solutions/lab14/src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import { CSSProperties } from 'react';
+import { useTheme } from '../hooks/useTheme';
 
 function Footer() {
+  const { theme, toggleTheme } = useTheme();
   const footerStyle: CSSProperties = {
     backgroundColor: 'black',
     color: 'white',
@@ -10,7 +12,13 @@ function Footer() {
     bottom: '0',
     width: '100%',
   };
-  return <p style={footerStyle}>This is the footer.</p>;
+
+  return (
+    <p style={footerStyle} className={theme === 'light' ? 'dark' : 'light'}>
+      This is the footer.
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </p>
+  );
 }
 
 export default Footer;
--- solutions/lab14/src/hooks/useTheme.jsx
+++ solutions/lab14/src/hooks/useTheme.jsx
@@ -2,9 +2,9 @@ import { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 export const useTheme = () => {
-  const { theme, useTheme } = useContext(ThemeContext);
+  const { theme, toggleTheme } = useContext(ThemeContext);
   if (!theme) {
     throw new Error('useTheme must be used within a ThemeProvider');
   }
-  return { theme, useTheme };
+  return { theme, toggleTheme };
 };
